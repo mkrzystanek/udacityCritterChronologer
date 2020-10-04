406e699acd6cc1a0ea3fab0bc5fd06572931c991
mkrzytsanek <mkrzystanek@gmail.com>
2020-10-04 11:22:26 +0200

    implement setting employee availability

--- src/main/java/com/udacity/jdnd/course3/critter/user/employee/EmployeeService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/employee/EmployeeService.java
@@ -3,6 +3,9 @@ package com.udacity.jdnd.course3.critter.user.employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
+import java.util.Set;
+
 @Service
 public class EmployeeService {
 
@@ -16,5 +19,11 @@ public class EmployeeService {
     public EmployeeEntity getEmployee(Long id) {
         return employeeRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Failed to find employee with id: " + id));
+    }
+
+    public void setAvailability(Set<DayOfWeek> daysAvailable, Long employeeId) {
+        EmployeeEntity employeeEntity = getEmployee(employeeId);
+        employeeEntity.setDaysAvailable(daysAvailable);
+        employeeRepository.save(employeeEntity);
     }
 }
